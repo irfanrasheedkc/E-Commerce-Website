005e701689b966fa6d968c44a8ed30264933f0f4
irfan rasheed <irfanrasheed273@gmail.com>
2022-11-25 22:09:34 +0530

    Basic structure
    Created a basic structure for front end and backend coding

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # E-Commerce-Website
  An e-commerce website using node js .
+
+$ express --hbs
+$ npm install
+$ npm install nodemon
+$ nodemon bin/www
---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let products=[
+    {
+      name:"IPhone 11",
+      category:"Mobile",
+      description:"The iPhone 11 has a 6.1-inch (15 cm) IPS LCD with a resolution is 1792 x 828 pixels (1.4 megapixels) at a pixel density of 326 PPI with a maximum brightness of 625 nits ",
+      image:"https://cellbuddy.in/buddy/wp-content/uploads/2022/09/11-purple.png"
+    },
+    {
+      name:"OnePlus Nord",
+      category:"Mobile",
+      description:"OnePlus phones are known for their solid battery life and fast charging support, which is why so many users prefer them. ",
+      image:"https://m.media-amazon.com/images/I/61+Q6Rh3OQL._SL1500_.jpg"
+    },
+    {
+      name:"Oppo 10X",
+      category:"Mobile",
+      description:"Seize the Night with Ultra Night Mode 2.0. The Reno 10x Zoom is equipped with the Qualcomm Snapdragon 855 processor.",
+      image:"https://cdn.dxomark.com/wp-content/uploads/medias/post-38804/oppo_reno_10xzoom.jpg"
+    },
+    {
+      name:"MI Note 9 Pro",
+      category:"Mobile",
+      description:"Image result for mi note 9 pro Xiaomi Redmi Note 9 Pro 5G flaunts 128GB internal storage which can be expanded up to 512GB. ",
+      image:"https://i02.appmifile.com/430_operator_in/30/01/2021/e95cb65d71fa8a1a5e7ee201e1a63d12!800x800!85.png"
+    }
+  ]
+  res.render('index', {products});
+});
+
+module.exports = router;

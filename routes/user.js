3754abdfc90c20e26e82ae2d245922adcc0ca4fe
irfan rasheed <irfanrasheed273@gmail.com>
2022-12-25 22:57:41 +0530

    Bug fix userLogin

--- routes/user.js
+++ routes/user.js
@@ -50,6 +50,7 @@ router.get('/signup', function (req, res) {
 router.post('/login', (req, res) => {
   userHelpers.doLogin(req.body).then((response) => {
     if (response.status) {
+      req.session.userLoggedIn = true
       req.session.user = response.user
       req.session.user.loggedIn = "Login failed"
       
